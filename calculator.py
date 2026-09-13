2f14615eae60fc2c0a5ced4784695cb352fd9f2d
David Freire <dhfreire20@gmail.com>
2026-09-13 06:45:43 +0000

    Add operation menu

--- calculator.py
+++ calculator.py
@@ -4,3 +4,10 @@ num1 = float(input("Enter the first number:"))
 num2 = float(input("Enter the second number:"))
 
 print("Numbers entered are:", num1, "and", num2)
+
+print("Select operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
